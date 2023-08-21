cfe678d11f35ce14c0c6a22a5961266ed5db031f
Marcos <marcos.vinicius.r.alencar@gmail.com>
2023-08-21 19:50:09 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
-from project import RecognitionAFNe as recognizer
-from project import ConvertERToAFNe as converter
+from project.AFNe.RecognitionAFNe import RecognitionAFNe
+from automataLib.fa.nfa import NFA
 
-conveObj = converter.ConvertERToAFNe()
-recogObj = recognizer.RecognitionAFNe()
+recogObj = RecognitionAFNe()
 
+# Reconhece +, e *
 expReg = input('Entre com sua expressão regular: ')
-conveObj.convertRegex(str(expReg))
+expReg = expReg.replace('+', '|')
+
+print(expReg)
+symbols = []
+for item in expReg:
+    teste = item in symbols 
+    if item != '|' and item != '*' and item != '()' and item != ')' and teste == False:
+        symbols.append(item)
+    
+
+nfa = NFA.from_regex(expReg, input_symbols=set(symbols))
+print(nfa)
+
+# conveObj.convertRegex(str(expReg))
+
+
+
 print('O autômato gerado tem os seguintes dados: ')
-conveObj.printInfo()
+
+separator = ', '
+separator2 = 'q'
+
+states = []
+for item in nfa.states:
+    states.append('q' + str(item)) 
+inicialState = 'q' + str(nfa.initial_state)
+finalStates = []
+for item in nfa.final_states:
+    finalStates.append('q' + str(item)) 
+for item in nfa.transitions:
+    print(item)
+print('Alfabeto: ' + str(list(nfa.input_symbols)))
+print('Estados: ' + str(states))
+print('Estado Inicial: ' + inicialState)
+print('Estado Final: ' + str(finalStates))
+print('Trasições: ')
+transicoes = {}
+for item in nfa.transitions:
+    print('q' + str(item) + ':', end='') 
+    
+    for item2 in nfa.transitions[item]:
+        
+        print(item2, end='') if item2 != '' else print('epsilon', end='')
+        
+        print(' --> ' + str(set(nfa.transitions[item][item2])))
+        
+        transicoes[(item, item2)] = set(nfa.transitions[item][item2])       
+print('\n')
 
 recogWord = input("Entre com a cadeia de caracteres: ")
+print('\n')
 
-verification = recogObj.VerificationAFe(conveObj.getStates(), conveObj.getAlphabet(), conveObj.getTransitions(), conveObj.getStartState(), conveObj.getFinalState(), recogWord)
+verification = RecognitionAFNe.VerificationAFe(list(nfa.states), symbols, transicoes, nfa.initial_state, nfa.final_states, recogWord)
 
 print('Palavra é aceita') if verification else print('Palavra não é aceita')
 
--- project/AFNe/RecognitionAFNe.py
+++ project/AFNe/RecognitionAFNe.py
@@ -1,27 +1,27 @@
+def closure( states, delta):
+        S = set(states)
+        notVisited = list(states)
+        while len(notVisited) > 0:
+            q = notVisited.pop()
+            if (q, '') in delta:
+                new = delta[(q, '')].difference(S)
+                S.update(new)
+                notVisited.extend(new)
+        return S
 class RecognitionAFNe():
 
     def __init__(self):
         self.message = "Recognizer AFNe"
 
     # Essa função avalia se uma cadeia é aceita ou não por um Autômato Finito Determinístico de Estado Vazios (AFDe)
-    def VerificationAFe(self, Q, sigma, delta, q0, F, chainOfCharacters):
-        activeStates = self.closure({q0}, delta)
+    def VerificationAFe( Q, sigma, delta, q0, F, chainOfCharacters):
+        activeStates = closure({q0}, delta)
         for symbol in chainOfCharacters:
             new = set()
             for q in activeStates:
                 if (q, symbol) in delta:
-                    new.update(self.closure( delta[(q, symbol)], delta))
+                    new.update(closure( delta[(q, symbol)], delta))
             activeStates = new
         return len(activeStates.intersection(F)) != 0
     # Função cria uma lista de estados que nao foram visitados para que estados repetidos nao sejam considerados,
     # dai verifica se e existe uma transicao vazia e ela leva a um estado que nao estava no fechamento e assim o estado passa a estar no fechamento
-    def closure( states, delta):
-        S = set(states)
-        notVisited = list(states)
-        while len(notVisited) > 0:
-            q = notVisited.pop()
-            if (q, 'epsilon') in delta:
-                new = delta[(q, 'epsilon')].difference(S)
-                S.update(new)
-                notVisited.extend(new)
-        return S
